af57e5b7aa4fdfaa1223db17723f39414cb20a38
AbdelrhmanAbuelnile <abdohani362@gmail.com>
2026-02-28 03:09:49 +0200

    chore: initial setup of shared config and types

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+// src/index.ts
+
+export enum UserRole {
+	CLIENT = "CLIENT",
+	CHEF = "CHEF",
+	ADMIN = "ADMIN",
+}
+
+export enum OrderStatus {
+	PENDING = "PENDING",
+	ACCEPTED = "ACCEPTED",
+	REJECTED = "REJECTED",
+	COOKING = "COOKING",
+	DELIVERING = "DELIVERING",
+	COMPLETED = "COMPLETED",
+}
+
+export interface IUser {
+	id: string;
+	email: string;
+	role: UserRole;
+	firstName: string;
+	lastName: string;
+	phoneNumber: string;
+	nationalId?: string;
+	nationalIdFaceImageUrl?: string;
+	nationalIdBackImageUrl?: string;
+	nationalIdSelfieImageUrl?: string;
+	nationalIdVerified?: boolean;
+	nationalIdExpiresAt?: Date;
+}
